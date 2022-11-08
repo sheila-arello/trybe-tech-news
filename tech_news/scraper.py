0540964f70848e891aaa7cdd93ade5b2729ab121
Sheila Arello <sheila@Sheilas-MacBook-Pro-2.local>
2022-11-08 17:02:46 -0300

    feat: scrape_next_page_link

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -32,14 +32,42 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    page = BeautifulSoup(html_content, "html.parser")
+    data = page.find("a", {"class": "next page-numbers"})
+    if not data:
+        next_page = None
+    else:
+        next_page = data.get("href")
+    return next_page
 
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    pass
+#     html_page = fetch(url)
+#     header_news = html_page.find("div", {"class": "entry-header-inner"})
+#     category = header_news.find("span", class_= "label").text
+#     title = header_news.find("h1", class_= "entry-title").text
+
+#     writer = header_news.find("span", class_= "author").text
+#     # tags = quote.find("div", class_="tags").find_all("a")
+#     # tag_list = [tag.text for tag in tags]
+#     # single_quote = [text, author, tag_list]
+#     data_news = {
+#                 "url": url,
+#                 "title": title,
+#                 "writer": writer,
+#                 "category": category,
+#               }
+#     return data_news
 
 
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+
+
+# if __name__ == "__main__":
+#     url = "https://blog.betrybe.com/"
+#     html_content = fetch(url)
+#     print(scrape_next_page_link(html_content))
