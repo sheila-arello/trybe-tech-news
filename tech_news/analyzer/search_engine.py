6e6a86a294fad3b15fbcf953bd479fd605a91531
Sheila Arello <sheila@Sheilas-MacBook-Pro-2.local>
2022-11-11 12:00:44 -0300

    feat: filtra noticias por data

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,3 +1,6 @@
+from datetime import date
+
+
 from tech_news.database import (
     search_news,
 )
@@ -5,23 +8,26 @@ from tech_news.database import (
 
 # Requisito 6
 def search_by_title(title):
-    # search = '/.*' + title + '.*/i'
     query = {"title": {"$regex": title, "$options": 'i'}}
     news = search_news(query)
     news_by_title = []
     for notice in news:
         news_by_title.append((notice['title'], notice['url']))
-# retornar uma lista de tuplas com as notícias encontradas
-#     [
-#   ("Título1_aqui", "url1_aqui"),
-#   ("Título2_aqui", "url2_aqui"),
-#     ]
     return news_by_title
 
 
 # Requisito 7
-def search_by_date(date):
-    """Seu código deve vir aqui"""
+def search_by_date(date_str):
+    try:
+        timestamp = date.fromisoformat(date_str).strftime('%d/%m/%Y')
+        query = {"timestamp": timestamp}
+        news = search_news(query)
+        news_by_date = []
+        for notice in news:
+            news_by_date.append((notice['title'], notice['url']))
+        return news_by_date
+    except Exception:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
